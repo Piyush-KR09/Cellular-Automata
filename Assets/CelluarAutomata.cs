1ea89e4942e0a70accb7bf2b048b4fffcca83ee0
Piyush-KR09 <piyushkumarraj380@gmail.com>
2025-03-19 11:58:12 +0530

    Added State Count Feature

--- Assets/CelluarAutomata.cs
+++ Assets/CelluarAutomata.cs
@@ -21,30 +21,32 @@ public class CellularAutomata : MonoBehaviour
     public float ImgScale;
 
     RandomNoiseGen noiseGen;
-    bool[,] map;
-    bool[,] finalMap;
+    bool[,] NonStateMap;
+    bool[,] NonStateFinalMap;
     int[,] stateMap;
     int[,] finalStateMap;
 
     private void Start()
     {
         noiseGen = GetComponent<RandomNoiseGen>();
-        map = noiseGen.MapGen(); // Get initial noise
+        NonStateMap = noiseGen.MapGen(); // Get initial noise
+        stateMap = new int[noiseGen.width, noiseGen.height];
 
         // Sets stateMap if use State is true
         if (useStates)
         {
-            for (int i = 0; i < map.GetLength(0); i++)
+            for (int i = 0; i < NonStateMap.GetLength(0); i++)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
+                for (int j = 0; j < NonStateMap.GetLength(1); j++)
                 {
-                    stateMap[i, j] = map[i, j] ? StateCounts : 0;
+                    stateMap[i, j] = NonStateMap[i, j] ? StateCounts : 0;
                 }
             }
+            //Generate State map
+            finalStateMap = CellularAutomataFunc(stateMap, generations);
         }
-
-        // Generate the map
-        finalMap = CellularAutomataFunc(map, generations);
+        //Generate Final map
+        else NonStateFinalMap = CellularAutomataFunc(NonStateMap, generations);
 
         //Data to ShowVisual()
         Texture2D texture2D = new (noiseGen.width, noiseGen.height);
@@ -56,6 +58,7 @@ public class CellularAutomata : MonoBehaviour
         displayImg.transform.localScale = Vector3.one * ImgScale;
     }
 
+    //used for !useStates
     bool[,] CellularAutomataFunc(bool[,] initialMap, int genLeft)
     {
         int width = initialMap.GetLength(0);
@@ -85,6 +88,38 @@ public class CellularAutomata : MonoBehaviour
             return newMap;
     }
 
+    //used for useStates
+    int[,] CellularAutomataFunc(int[,] initialState, int genLeft)
+    {
+        int width = initialState.GetLength(0);
+        int height = initialState.GetLength(1);
+
+        // Create a copy of the map
+        int[,] newState = new int[width, height];
+
+        for (int i = 1; i < width - 1; i++)
+        {
+            for (int j = 1; j < height - 1; j++)
+            {
+
+                int aliveNeighbors = CountAliveNeighbors(initialState, i, j);
+
+                // Apply survival and birth rules
+                if (aliveNeighbors >= survivalCount)
+                    newState[i, j] = Mathf.Min(initialState[i, j] + 1, StateCounts); // Strengthen wall
+                else
+                    newState[i, j] = Mathf.Max(initialState[i, j] - 1, 0); // Weaken wall
+            }
+        }
+
+        // If more generations are left, recurse
+        if (genLeft > 0)
+            return CellularAutomataFunc(initialState, genLeft - 1);
+        else
+            return newState;
+    }
+    
+    //Takes Bool
     int CountAliveNeighbors(bool[,] map, int x, int y)
     {
         int count = 0;
@@ -114,30 +149,82 @@ public class CellularAutomata : MonoBehaviour
         {
             int nx = neighbors[i, 0], ny = neighbors[i, 1];
             if (nx >= 0 && ny >= 0 && nx < map.GetLength(0) && ny < map.GetLength(1))
-                if (map[nx, ny]) count++;
+            {
+                if (map[nx, ny])
+                    count++;
+            }
         }
 
         return count;
-    } 
+    }
+    //Takes Ink
+    int CountAliveNeighbors(int[,] map, int x, int y)
+    {
+        int count = 0;
+        int[,] neighbors = useMoore ? new int[,]
+        {
+            { x - 1, y + 1 },
+            { x, y + 1 },
+            { x + 1, y + 1 },
+
+            { x - 1, y },
+            { x + 1, y },
+
+            { x - 1, y - 1 },
+            { x, y - 1 },
+            { x + 1, y - 1 }
+
+        } : new int[,]
+        {
+            { x, y + 1 },
+            { x, y - 1 },
+
+            { x + 1, y },
+            { x - 1, y }
+        };
+
+        for (int i = 0; i < neighbors.GetLength(0); i++)
+        {
+            int nx = neighbors[i, 0], ny = neighbors[i, 1];
+            if (nx >= 0 && ny >= 0 && nx < map.GetLength(0) && ny < map.GetLength(1))
+                if (map[nx, ny] > 0) count++;
+        }
+
+        return count;
+    }
 
     private void ShowVisuals(Texture2D texture2D)
     {
         displayImg.rectTransform.sizeDelta = new(noiseGen.width, noiseGen.height);
         displayImg.texture = texture2D;
 
-        if (map != null && finalMap != null)
+        if (useStates && finalStateMap!= null)
         {
-            int width = map.GetLength(0), height = map.GetLength(1);
+            int width = finalStateMap.GetLength(0), height = finalStateMap.GetLength(1);
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    texture2D.SetPixel(i, j, finalMap[i, j] ? baseColor : wallColor);
+                    float stateValue = Random.Range(0, StateCounts + 1) / (float)StateCounts; // Normalize (0 to 1)
+                    var pixelColor = Color.Lerp(wallColor,baseColor,stateValue) ;
+                    texture2D.SetPixel(i, j, pixelColor);
+                }
+            }            
+        }
+        else if (!useStates && NonStateFinalMap != null)
+        {
+            int width = NonStateFinalMap.GetLength(0), height = NonStateFinalMap.GetLength(1);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    texture2D.SetPixel(i, j, NonStateFinalMap[i, j] ? baseColor : wallColor);
                 }
             }
-            texture2D.filterMode = FilterMode.Point;
-            texture2D.wrapMode = TextureWrapMode.Clamp;
-            texture2D.Apply();
         }
+
+        texture2D.filterMode = FilterMode.Point;
+        texture2D.wrapMode = TextureWrapMode.Clamp;
+        texture2D.Apply();
     }
 }
